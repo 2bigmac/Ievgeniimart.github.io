4c9fd6761a04d0bd3124c2f97a6342a5599c642b
IvI <ievgeniimart@gmail.com>
2016-11-08 23:57:07 +0200

    correction

--- scripts/home.js
+++ scripts/home.js
@@ -172,7 +172,7 @@ jQuery(document).ready(function(){
 /* ==========  START GOOGLE MAP ========== */
 
 // When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
+var google.maps.event.addDomListener(window, 'load', init);
 
 function init() {
 	// Basic options for a simple Google Map
